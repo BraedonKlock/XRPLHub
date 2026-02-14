389cb2dbcc04d581456c44969f2289e1b3e21baf
Braedon Klock <braedonk@hotmail.com>
2026-02-14 09:23:20 -0500

    CREATED: Header.jsx and set up its route in the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 
 import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header.jsx";
 
 import LoggedinHomePage from './pages/LoggedinHomePage.jsx';
 function App() {
 
   return (
     <>
+	  <Header />
 	  <Routes>
 	  	<Route path="/home" element={<LoggedinHomePage/>}/>
 	  </Routes>
