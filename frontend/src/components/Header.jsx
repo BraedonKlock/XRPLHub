0a8193f2ee6007202126027825779bcc94206072
Braedon Klock <braedonk@hotmail.com>
2026-02-18 10:34:07 -0500

    IMPLEMENTED: isOpen state for mobile nav menu in Header.jsx and corresponding css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,24 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import {useState} from "react";
+import { NavLink, Link } from "react-router-dom";
 
 import "./Header.css";
 
 export default function Header() {
+	const [isOpen, setIsOpen] = useState(false);
+
 	return (
 		<header>
 			<nav>
-				<button className="hamburger">
+				<button onClick={() => setIsOpen(prev => !prev)} className="hamburger">
 					<FontAwesomeIcon icon={faBars} size="lg" />
 				</button>
 			</nav>
 			<h1>Header</h1>
+			<div className={`mobile ${isOpen ? "open" : ""}`}>
+				<NavLink>option one</NavLink>
+			</div>
 		</header>
 	)
 }
