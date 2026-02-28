87cd7a5a19a073345cb56ed432bc4126a7a5589c
Braedon Klock <braedonk@hotmail.com>
2026-02-28 16:36:39 -0500

    IMPLEMENTED: useEffect with handleCLickOutside function and useRef for collapsing nav menu when use cicks outside the header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,31 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-import {useState} from "react";
+import {useState, useRef, useEffect} from "react";
 import { NavLink, Link } from "react-router-dom";
 
 import "./Header.css";
 
 export default function Header() {
 	const [isOpen, setIsOpen] = useState(false);
+	const menuRef = useRef(null);
+	
+	useEffect(() => {
+		function handleClickOutside(e) {
+			if (!menuRef.current) return;
+			if (!menuRef.current.contains(e.target)) {
+				setIsOpen(false);
+			};
+		};
+
+		document.addEventListener("pointerdown", handleClickOutside, true);
+
+		return () => {
+			document.removeEventListener("pointerdown", handleClickOutside, true);
+		};
+	},[])
 
 	return (
-		<header>
+		<header ref={menuRef}>
 			<nav>
 				<button onClick={() => setIsOpen(prev => !prev)} className="hamburger">
 					<FontAwesomeIcon icon={faBars} size="lg" />
@@ -17,7 +33,7 @@ export default function Header() {
 			</nav>
 			<h1>Header</h1>
 			<div className={`mobile ${isOpen ? "open" : ""}`}>
-				<NavLink>option one</NavLink>
+				<NavLink to="">option one</NavLink>
 			</div>
 		</header>
 	)
