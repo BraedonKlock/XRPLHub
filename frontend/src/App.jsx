2c11debdc29642da48942f5049a864189486ef51
Braedon Klock <braedonk@hotmail.com>
2026-03-01 17:39:36 -0500

    CREATED: HomePage.jsx/.css for not logged in users and LoginPage.jsx/.css and set up the routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import ShootingStarCursor from "./components/ShootingStarCursor.jsx";
 
+import HomePage from './pages/HomePage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
 import DashboardPage from './pages/DashboardPage.jsx';
 import WalletCreationPage from './pages/WalletCreationPage.jsx';
 
@@ -14,6 +16,8 @@ function App() {
       <ShootingStarCursor />
 	  <Header />
 	  <Routes>
+	  	<Route path="/" element={<HomePage/>}/>
+	  	<Route path="/login" element={<LoginPage/>}/>
 	  	<Route path="/dashboard" element={<DashboardPage/>}/>
 	  	<Route path="/wallet-creation" element={<WalletCreationPage/>}/>
 	  </Routes>
